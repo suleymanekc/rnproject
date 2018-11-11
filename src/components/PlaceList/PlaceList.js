41d90e2b38ab175079f4ffe3deee200eb64be82b
emre cavunt <emre.cavunt@gmail.com>
2018-11-11 21:37:15 +0300

    First Commit

---
+++ src/components/PlaceList/PlaceList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View ,StyleSheet,FlatList}  from 'react-native';
+import ListItem from '../ListItem/ListItem';
+
+const placeList = props => {
+    const placesOutput = props.places.map((place,i)=> (
+        <ListItem key={i} placeName={place} onItemPressed={()=>props.onItemDeleted(i)} />
+      ));
+    return (
+      <FlatList  style={styles.listContainer} >   
+         {placesOutput}
+       </FlatList>
+    );
+}; 
+const styles = StyleSheet.create({
+    listContainer:{
+        width:"100%"
+    }   
+});
+export default placeList;
